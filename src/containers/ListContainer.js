00556eccee8f5b9c77cebfb3b71d06bda2884c5f
ykwimk <ykwimk@gmail.com>
2020-09-28 12:15:18 +0900

    Refactor) hooks 교체중..

--- src/containers/ListContainer.js
+++ src/containers/ListContainer.js
@@ -1,10 +1,41 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import _ from 'lodash';
-import { observer, inject } from 'mobx-react';
+import { observer } from 'mobx-react';
 import Header from '../components/Header/Header';
 import ProductList from '../components/ProductList/ProductList';
 import WishList from '../components/WishList/WishList';
+import useStores from '../useStores';
 
+const ListContainer = observer(() => {
+  const { list } = useStores()
+  const targetRef = useRef(null)
+
+  const handleIntersection = () => {
+    if (list.page === 'product') {
+      list.increaseData()
+      list.getListData()
+    }
+  }
+
+  useEffect(() => {
+    list.getListData()
+    .then(() => {
+      const io = list.intersectionObserver(handleIntersection)
+      io.observe(targetRef)
+    })
+    .catch(err => console.log(err))
+  }, [])
+
+  return (
+    <>
+      <Header />
+      {list.page === 'product' ? <ProductList /> : <WishList /> }
+      {list.isTargetRef && <div ref={targetRef}></div>}
+    </>
+  )
+})
+
+/*
 @inject(stores => ({
   page: stores.list.page,
   isTargetRef: stores.list.isTargetRef,
@@ -44,5 +75,6 @@ class ListContainer extends React.Component {
     )
   }
 }
+*/
 
 export default ListContainer;
